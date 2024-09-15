6eb48629456a14e28125455919effdc3bb335af5
Lindokuhle Ntuli <lindokuhlentuli8@gmail.com>
2024-09-15 13:58:03 +0200

    basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+# A Python script that performs basic arithmetic operations with two predefined numbers
+number1 = int(10)
+number2 = int(5)
+
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+print("addition of number1 and number2 is",addition)
+print("subtraction of number1 and number2 is",subtraction)
+print("multiplication of number1 and number2 is",multiplication)
