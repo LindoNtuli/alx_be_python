03dd251974a0f9e88e879203b5668aa12aecc7e8
Lindokuhle Ntuli <lindokuhlentuli8@gmail.com>
2024-09-15 14:18:25 +0200

    simple_interest.py

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+# To complete a Python script that calculates the simple interest earned on an investment over a period of time
+principal = 1000
+rate = 0.05
+time = 3
+
+interest = principal * rate * time
+
+print("The simple interest is:", interest)
