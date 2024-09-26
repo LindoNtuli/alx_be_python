524f9101d5fab0d20a19aaa273df75a19dd9a377
Lindokuhle Ntuli <lindokuhlentuli8@gmail.com>
2024-09-26 10:10:59 +0200

    pattern_drawing.py

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -6,3 +6,14 @@ for i in range(size):
         print('*', end=' ')
     print()
     # Move to the next line after each row
+def main():
+    while True:
+        try: user_input = int(input("Enter a positive integer: "))
+        except ValueError:
+                print("Invalid input. Please enter a positive integer.")
+                if __name__ == "__main__": main()
+
+    if user_input > 0: draw_square_pattern(user_input)
+
+
+            
