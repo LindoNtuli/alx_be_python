5326bb7bb7daf88f5dedfab0a71171c76fe7a585
Lindokuhle Ntuli <lindokuhlentuli8@gmail.com>
2024-09-29 16:20:13 +0200

    daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -16,4 +16,4 @@ match Priority:
 # Modify the reminder if the task is time-bound
 if Time_bound == "yes": Reminder += ""
 # Print the customized reminder
-print (Reminder)
+print (f"Reminder, {Reminder}!")
