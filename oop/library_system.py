c1ce46d829dfb44729f11a18d9b765c74f92505c
Lindokuhle Ntuli <lindokuhlentuli8@gmail.com>
2024-10-13 19:54:37 +0200

    library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,17 +1,17 @@
 # library_system.py
 
 class Book:
-    def __init__(self, title, author):
+    def __init__(self, title: str, author: str):
         self.title = title
         self.author = author
 
 class EBook(Book):
-    def __init__(self, title, author, file_size: int):
+    def __init__(self, title: str, author: str, file_size: int):
         super().__init__(title, author)
         self.file_size = file_size
 
 class PrintBook(Book):
-    def __init__(self, title, author, page_count: int):
+    def __init__(self, title: str, author: str, page_count: int):
         super().__init__(title, author)
         self.page_count = page_count
 
